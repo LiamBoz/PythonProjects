776ff7b2409058b224a9131ee1c74d3549d10099
Liam <thisamazingnow@gmail.com>
2022-11-02 22:38:29 -0400

    GREP MORE

--- grep/grep.py
+++ grep/grep.py
@@ -9,6 +9,15 @@
 # get the text contents of a file -
 ## input   the file    (file handle)
 ## output   all the text in the file    (a list of strings)
+
+# get text input from user
+## input the text (string)
+## output the text (string)
+
+# search lines for that text
+## input the text (string)
+## output the line number (integer? string?)
+
 print("starting the grep program")
 def GetFileName():
     filename = input("File Name:")
@@ -22,9 +31,21 @@ def GetTextContents(FileHandle):
     lines = FileHandle.readlines()
     return lines
 
+def GetSearchText():
+    searchtext = input("Enter the text you are looking for:")
+    return searchtext
+
+def SearchForText(searchtext, lines):
+    for line in lines:
+        if (searchtext in line):
+            print(line)
+
+    # look for 'searchtext' in 'lines'
+
 filename = GetFileName()
 FileHandle = FindFile(filename)
 lines = GetTextContents(FileHandle)
+searchtext = GetSearchText()
+SearchForText(searchtext, lines)
+
 print(lines)
-
-
